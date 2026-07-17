0c4f38b011bb1f09e2cad87e802389a8bac863bb
Roi Yacov <royacov8@gmail.com>
2026-07-17 11:50:44 +0300

    feat: add task creation endpoint with request validation

--- app/backend/main.py
+++ app/backend/main.py
@@ -1,6 +1,24 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+
+class TaskCreate(BaseModel):
+    title: str
+
+tasks = [
+    {
+        "id": 1,
+        "title": "Learn FastAPI",
+        "completed": False
+    },
+    {
+        "id": 2,
+        "title": "Learn Docker",
+        "completed": True
+    }
+]
 
 
 @app.get("/health")
@@ -10,15 +28,16 @@ def health():
 
 @app.get("/tasks")
 def get_tasks():
-    return [
-        {
-            "id": 1,
-            "title": "Learn FastAPI",
-            "completed": False
-        },
-        {
-            "id": 2,
-            "title": "Learn Docker",
-            "completed": True
-        }
-    ]
+    return tasks
+
+
+@app.post("/tasks", status_code=201)
+def create_task(task: TaskCreate):
+    new_task = {
+        "id": len(tasks) + 1,
+        "title": task.title,
+        "completed": False
+    }
+
+    tasks.append(new_task)
+    return new_task
