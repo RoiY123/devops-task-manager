df25a632db16a4b189e923722214b0cfc640a70f
Roi Yacov <royacov8@gmail.com>
2026-08-31 20:44:17 +0300

    fix: update GitHub OIDC subject claim

--- terraform/github-actions.tf
+++ terraform/github-actions.tf
@@ -39,7 +39,7 @@ data "aws_iam_policy_document" "github_actions_assume_role" {
       variable = "token.actions.githubusercontent.com:sub"
 
       values = [
-        "repo:RoiY123/devops-task-manager:ref:refs/heads/main"
+        "repo:RoiY123@252551300/devops-task-manager@1303006579:ref:refs/heads/main"
       ]
     }
   }
